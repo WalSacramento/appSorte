a2e1b3568a78e980f5fbbbdf2fecc52dba159fca
Waldsson Sacramento <waldsson.dev@gmail.com>
2024-01-25 11:42:56 -0300

    Add colors.js file with color definitions

---
+++ src/styles/colors.js
@@ -0,0 +1,9 @@
+export default {
+  vermelho: '#FF0000',
+  amarelo: '#FFD700',
+  cinza: '#d9d9d9',
+  pretoTexto: '#333333',
+  brancoFundos: '#f9f9f9',
+  verde: '#2ECC71',
+  laranja: '#FFA500',
+};
